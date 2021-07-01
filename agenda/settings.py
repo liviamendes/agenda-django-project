abfdc316126d5b9bf9685e02c933bd3b147eda80
Lívia Mendes <mendes.souza.livia@gmail.com>
2021-07-01 19:24:36 +0100

    add css admin area

--- agenda/settings.py
+++ agenda/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y0d-(o4qcn@%)%iy*gwe9c7lfmz#14+0h(juxup^s_1nj)1#u@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
